7a1966e897839494c43ec6d42fa74322874857af
OleksiiMinnikov <89975657+OleksiiMinnikov@users.noreply.github.com>
2023-04-24 21:41:24 +0300

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,12 +3,16 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+  
+
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle("modal-open");
   }
 })();
