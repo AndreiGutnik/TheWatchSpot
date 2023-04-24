6c33904ed7807391ee80e40220dc598dccd8aab3
Yuliia001 <babeyuliia@gmail.com>
2023-04-24 07:38:22 +0200

    changes

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,15 +1,34 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
   
-  menuBtnRef .addEventListener('click', ()  => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//   menuBtnRef .addEventListener('click', ()  => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
     
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
 
+
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelectorAll('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.forEach((el) => {
+    el.addEventListener('click', toggleMenu);
+  });
+
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+  }
+})();
