6f7ed986107ca03b7452eee5b185d6df41343695
Yuliia001 <babeyuliia@gmail.com>
2023-04-24 09:55:05 +0200

    update

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,23 +1,10 @@
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-  
-//   menuBtnRef .addEventListener('click', ()  => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-    
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
-
-
 (() => {
   const refs = {
     openMenuBtn: document.querySelectorAll('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
+    closeMenuLink1: document.querySelector('[data-menu-close1]'),
+    closeMenuLink2: document.querySelector('[data-menu-close2]'),
+    closeMenuLink3: document.querySelector('[data-menu-close3]'),
     menu: document.querySelector('[data-menu]'),
   };
 
@@ -26,6 +13,9 @@
   });
 
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuLink1.addEventListener('click', toggleMenu);
+  refs.closeMenuLink2.addEventListener('click', toggleMenu);
+  refs.closeMenuLink3.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-open');
